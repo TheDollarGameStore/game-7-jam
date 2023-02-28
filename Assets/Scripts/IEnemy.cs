55f34db4c56e62e0be10dd0a240965c035c46436
TheDollarGameStore <thedollargamestore@gmail.com>
2023-02-28 20:01:57 +0200

    Stuff

--- Assets/Scripts/IEnemy.cs
+++ Assets/Scripts/IEnemy.cs
@@ -19,14 +19,26 @@ public class IEnemy : MonoBehaviour
 
     [SerializeField] private GameObject starPrefab;
 
-    [SerializeField] private Text hpText;
+    private Text hpText;
+
+    [SerializeField] private GameObject hpCounterPrefab;
+
+    private GameObject hpCounter;
+
+    [SerializeField] private float hpCounterYOfffset;
 
     // Start is called before the first frame update
     public void Start()
     {
         mr = GetComponent<MeshRenderer>();
         defaultMaterial = mr.material;
-        hpText.text = hp.ToString();
+
+        if (hpCounterPrefab != null)
+        {
+            hpCounter = Instantiate(hpCounterPrefab, transform.position, Quaternion.identity);
+            hpText = hpCounter.GetComponentInChildren<Text>();
+            hpText.text = Mathf.CeilToInt(hp).ToString();
+        }
     }
 
     // Update is called once per frame
@@ -44,7 +56,10 @@ public class IEnemy : MonoBehaviour
         }
         else
         {
-            hpText.text = Mathf.CeilToInt(hp).ToString();
+            if (hpText != null)
+            {
+                hpText.text = Mathf.CeilToInt(hp).ToString();
+            }
         }
     }
 
@@ -65,6 +80,7 @@ public class IEnemy : MonoBehaviour
             Instantiate(starPrefab, new Vector3(transform.position.x, 1.75f, transform.position.z), Quaternion.identity).GetComponent<Star>().direction = new Vector3(Random.Range(-1, 1f), 0f, Random.Range(-1, 1f)).normalized * 10f;
         }
 
+        Destroy(hpCounter);
         Destroy(gameObject);
     }
 
@@ -75,6 +91,11 @@ public class IEnemy : MonoBehaviour
 
     public void Update()
     {
+        if (hpCounter != null)
+        {
+            hpCounter.transform.position = transform.position + (Vector3.up * hpCounterYOfffset);
+        }
+
         if (Vector3.Distance(new Vector3(transform.position.x, 0f, transform.position.z), GameManager.instance.playerObject.transform.position) <= 1.75f)
         {
             GameManager.instance.playerObject.GetComponent<PlayerBehaviour>().Die();
